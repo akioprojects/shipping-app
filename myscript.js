d47b9c053e57fa4d40db849e8221dbcaee7899ac
Rustlingleaves <ivan.lapickij@gmail.com>
2023-02-16 19:22:23 +0000

    Merged Login + content/search

--- myscript.js
+++ myscript.js
@@ -43,3 +43,84 @@ function mycontent() {
 	  x.style.display = "block";
 	}
   }
+
+// +++++++++++++++++++++BOOTSTRAP CODE
+rootUrl = 'http://localhost:9091/';
+
+
+
+function retailurelogin() {
+	console.log('Charlotte show/hide default');
+	document.getElementById('login').style.display ="none";
+	document.getElementById('LogOut').style.display ="";
+	document.getElementById('lastOrder').style.display ="";
+	document.getElementById('basket').style.display ="";
+	document.getElementById('Shopping').style.display ="";
+	document.getElementById('home').style.display ="none";
+	
+	
+}
+
+function logout(){
+	console.log('Charlotte show/hide logout');
+	document.getElementById('LogOut').style.display ="none";
+	document.getElementById('lastOrder').style.display ="none";
+	document.getElementById('basket').style.display ="none";
+	document.getElementById('Shopping').style.display ="none";
+	document.getElementById('home').style.display ="";
+	document.getElementById('login').style.display ="";
+	
+}
+
+function shopping(){
+	console.log('Charlotte show/hide shopping');
+	document.getElementById('Shopping').style.display ="none";
+	document.getElementById('addStock').style.display ="";
+}
+function login(){
+	console.log('Charlotte show/hide shopping');
+	document.getElementById('Shopping').style.display ="none";
+	document.getElementById('addStock').style.display ="";
+}
+
+addclothes = function() {
+
+	console.log('addclothes');
+	$.ajax({
+		type: 'POST',
+		contentType: 'application/json',
+		url: rootUrl + 'clothes',
+		dataType: 'json',
+		data: formToJSON(),
+		success: function(data, textStatus, jpXHR) {
+			alert('Clothes created successfully');
+			findAll();
+
+		},
+
+		error: function(jpXHR, textStatus, errorThrown) {
+
+			alert('addClothes error' + textStatus);
+		
+		}
+
+	});
+	}
+	function mysearch() {
+		var input, filter, table, tr, td, i, txtValue;
+		input = document.getElementById("myInput");
+		filter = input.value.toUpperCase();
+		table = document.getElementById("myTable");
+		tr = table.getElementsByTagName("tr");
+		for (i = 0; i < tr.length; i++) {
+		  td = tr[i].getElementsByTagName("td")[0];
+		  if (td) {
+			txtValue = td.textContent || td.innerText;
+			if (txtValue.toUpperCase().indexOf(filter) > -1) {
+			  tr[i].style.display = "";
+			} else {
+			  tr[i].style.display = "none";
+			}
+		  }       
+		}
+	  }
